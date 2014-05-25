0dadb49434a934bad3fe560c0e6e2aa51d3ae8a5
Viet <vmai07@gmail.com>
2014-05-25 17:11:59 +0800

    commit CacheMatrix.R - attempt to push

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Create a list of functions: set,get,setinverse, getinverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        #instantiate object im to store the inverse of matrix x
+        im <- NULL
+        set <- function(y){
+                 x <<- y
+                 im <<- NULL
+         }
+        get <- function() x
+        setinverse <- function(inverse) im <<- inverse
+        getinverse <- function() im
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Cache the inverse of matrix x
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        im <- x$getinverse()
+        if(!is.null(im)){
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data,...)
+        x$setinverse(im)
+        im
 }
+
